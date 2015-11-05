d0b5ee97379db78805bfb9e31de482e590e9d58a
mshirdel <mshirdel@gmail.com>
2015-11-05 12:31:52 +0330

    get posts list with angular

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,24 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+var host = location.protocol.concat("//").concat(window.location.hostname).concat(":").concat(window.location.port);
+var app = angular.module('rails_blog', []);
+app.controller('blogCtrl', function($scope, $http){
+    
+    $http.get(host + "/posts.json").success(function(response){        
+        $scope.all_post = response;
+    });
+    
+    $scope.postDetails = function(id){
+        window.location = host + "/posts/" + id;
+    }
+    
+    $scope.shortText = function(content){
+        var maxLength = 300; // maximum number of characters to extract
+        //trim the string to the maximum length
+        var trimmedString = content.substr(0, maxLength);
+        //re-trim if we are in the middle of a word
+        trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")));
+        return trimmedString + " ... ";
+    }
+});
