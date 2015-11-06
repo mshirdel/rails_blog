a8e2c88c3e7a74d0a0fdc63ca98fdc90c0c972a7
mshirdel <mshirdel@gmail.com>
2015-11-06 09:14:42 +0330

    add devise gem for user authentication

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module RailsBlog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.i18n.default_locale = :fa
   end
 end
